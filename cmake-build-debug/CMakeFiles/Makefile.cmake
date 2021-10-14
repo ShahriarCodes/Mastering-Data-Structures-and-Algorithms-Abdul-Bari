8abf5321b6b6f841e52a1e0cb95c5ce27f800978
shahriar100 <shahriar.hasan100@gmail.com>
2021-10-14 22:04:40 +0600

    indirect recursion

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -46,6 +46,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/indirect_recursion.dir/DependInfo.cmake"
   "CMakeFiles/head_recursion.dir/DependInfo.cmake"
   "CMakeFiles/tail_recursion.dir/DependInfo.cmake"
   "CMakeFiles/static_global_variables.dir/DependInfo.cmake"
---
+++ section05_recursion/indirect_recursion.cpp
@@ -0,0 +1,29 @@
+//
+// Created by sal on 14.10.21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void funA(int n);
+void funB(int n);
+
+void funA(int n) {
+    if (n > 0) {
+        cout << n << " ";
+        funB(n - 1);
+    }
+}
+
+void funB(int n) {
+    if (n > 1) {
+        cout << n << " ";
+        funA(n / 2);
+    }
+}
+
+int main() {
+    funA(20);
+}
+
