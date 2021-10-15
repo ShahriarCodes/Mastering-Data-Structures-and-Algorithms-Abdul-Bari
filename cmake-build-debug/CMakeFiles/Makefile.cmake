98f85238f8995bb2e8f7c7aa86c920a09b9052dc
shahriar100 <shahriar.hasan100@gmail.com>
2021-10-16 00:43:50 +0600

    factorial of a number with recursion

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -46,6 +46,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "CMakeFiles/factorial_of_a_number.dir/DependInfo.cmake"
   "CMakeFiles/nested_recursion.dir/DependInfo.cmake"
   "CMakeFiles/indirect_recursion.dir/DependInfo.cmake"
   "CMakeFiles/head_recursion.dir/DependInfo.cmake"
---
+++ section05_recursion/factorial_of_a_number.cpp
@@ -0,0 +1,29 @@
+//
+// Created by sal on 16.10.21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// n! = 1 * 2 * 3 * 4 * 5 * .... * n
+// n! = 1 * 2 * 3 * 4 * 5 * .... * (n-1) * n
+// fact(n) = 1 * 2 * 3 * 4 * 5 * .... * (n-1) * n
+// fact(n) = fact(n-1) * n
+
+// recursive definition:
+// fact(n) = { 1                -> n = 0
+//           { fact(n-1) * n    -> n > 0
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return factorial(n - 1) * n;
+    }
+}
+
+int main() {
+    cout << factorial(6) << endl;
+}
+
