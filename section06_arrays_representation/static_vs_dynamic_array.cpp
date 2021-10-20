9070073b88e437c7a58cfa3ff9a26d88af270075
shahriar100 <shahriar.hasan100@gmail.com>
2021-10-21 00:03:06 +0600

    increasing a dynamic array size

--- section06_arrays_representation/static_vs_dynamic_array.cpp
+++ section06_arrays_representation/static_vs_dynamic_array.cpp
@@ -11,7 +11,7 @@ int main() {
     int *p;
 
     // heap memory can only be accessed with pointer
-    // creating array in heap
+    // todo: creating array in heap
     p = new int[5]; // c++
     p = (int *) malloc(5 * sizeof(int)); // c lang
 
@@ -26,6 +26,24 @@ int main() {
     // avoid memory leak problem
     delete[]p;
     // free(p); // clang
+
+
+    // Todo: increasing a dynamic array size with another pointer
+    p = new int[5];
+    int *q = new int[10];
+
+    p[0] = 5;
+    p[2] = 7;
+    p[3] = 6;
+
+    // copying the values of p to q
+    for (int i = 0; i < 5; i++) {
+        q[i] = p[i];
+    }
+
+    delete[] p;
+    p = q;
+    q = nullptr; // removing q from pointing to array
 
 
 }
