df4f43fd3a23ff5145e11705b1dbb89892a6a756
shahriar100 <shahriar.hasan100@gmail.com>
2021-10-22 20:06:45 +0600

    increasing a dynamic array size clang

--- section06_arrays_representation/2d_array.cpp
+++ section06_arrays_representation/2d_array.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main() {
     int A[][3] = {1, 2, 3, 4, 5, 6};
-    cout << *A[0]<< endl;
+    cout << *A[1] << endl;
 
     // array of pointers
     int *B[3];
@@ -18,14 +18,25 @@ int main() {
     B[1] = new int[5];
     B[2] = new int[7];
 
+    // for clang
+    B[0] = (int *) malloc(4 * sizeof(int));
+    B[1] = (int *) malloc(4 * sizeof(int));
+    B[2] = (int *) malloc(4 * sizeof(int));
+
 
     // method 3 of creating array
     int **C;
-    // array of pointers created and C is pointing to the array
+    // array of pointers created in heap and C is pointing to the array
     C = new int *[3];
     C[0] = new int[4];
     C[1] = new int[4];
     C[2] = new int[4];
+
+    // for clang
+    C = (int **) malloc(3 * sizeof(int *));
+    C[0] = (int *) malloc(4 * sizeof(int));
+    C[1] = (int *) malloc(4 * sizeof(int));
+    C[2] = (int *) malloc(4 * sizeof(int));
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++) {
