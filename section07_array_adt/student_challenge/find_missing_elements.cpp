3be186919dd8c96ef82b5f3727d3d72737358651
shahriar100 <shahriar.hasan100@gmail.com>
2021-12-03 21:49:19 +0600

    find missing element 2nd approach

--- section07_array_adt/student_challenge/find_missing_elements.cpp
+++ section07_array_adt/student_challenge/find_missing_elements.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// array should be sorted
 int find_missing(int A[], int n) {
     int sum = 0;
     for (int i = 0; i < n - 1; i++) sum += A[i];
@@ -13,8 +14,20 @@ int find_missing(int A[], int n) {
     return s - sum;
 }
 
+int find_missing2(int A[], int n) {
+    int low = A[0], high, diff;
+    for (int i = 0; i < n; i++) {
+        high = A[i];
+        if (diff = high - i != low) return high - 1;
+    }
+    return -1;
+}
+
 int main() {
     int A[11] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12};
-    cout << "Missing no is: " << find_missing(A, 12);
+    cout << "Missing no is: " << find_missing(A, 12) << endl;
+
+    int A2[6] = {6, 7, 8, 9, 11, 12};
+    cout << "Missing no is: " << find_missing2(A2, 6) << endl;
 }
 
