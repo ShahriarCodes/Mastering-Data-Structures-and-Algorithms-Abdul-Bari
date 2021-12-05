9af047a79a7564deabeeb18db070f094d701c1b4
shahriar100 <shahriar.hasan100@gmail.com>
2021-12-05 21:54:35 +0600

    find multiple missing elements 2nd approach

--- section07_array_adt/student_challenge/find_missing_elements.cpp
+++ section07_array_adt/student_challenge/find_missing_elements.cpp
@@ -18,10 +18,44 @@ int find_missing2(int A[], int n) {
     int low = A[0], high, diff;
     for (int i = 0; i < n; i++) {
         high = A[i];
-        if (diff = high - i != low) return high - 1;
+        diff = high - i;
+        if (diff != low) return high - 1;
     }
     return -1;
 }
+
+void find_multiple_missing(int A[], int n) {
+    int diff = A[0] - 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] - i != diff) {
+            while (diff < A[i] - i) {
+                cout << i + diff << " ";
+                diff++;
+            }
+        };
+    }
+}
+
+void find_multiple_missing2(int A[], int n, int low, int high) {
+    int B[high];
+    // initialize the array(hashtable) with zeros
+    for (int i = 0; i <= high; i++) {
+        B[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        B[A[i]]++;
+    };
+
+    for (int i = 1; i <=high; i++) {
+        if(B[i] == 0) {
+            cout << i << " ";
+        }
+    };
+}
+
+
+
 
 int main() {
     int A[11] = {1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12};
@@ -29,5 +63,15 @@ int main() {
 
     int A2[6] = {6, 7, 8, 9, 11, 12};
     cout << "Missing no is: " << find_missing2(A2, 6) << endl;
+
+    int A3[6] = {6, 9, 12, 18, 20, 21};
+    cout << "Missing no are: ";
+    find_multiple_missing(A3, 6);
+    cout << endl;
+
+    int A4[10] = {3,7,4,9,12,6,1,11,2,10};
+    cout << "Missing no are: ";
+    find_multiple_missing2(A4, 10, 1, 12);
+    cout << endl;
 }
 
