1279f908d54078b09d9e7ad9affb3eb48666417b
shahriar100 <shahriar.hasan100@gmail.com>
2021-12-06 21:41:55 +0600

    find duplicate count

--- section07_array_adt/student_challenge/find_duplicate_elements.cpp
+++ section07_array_adt/student_challenge/find_duplicate_elements.cpp
@@ -17,10 +17,29 @@ void find_duplicates(int A[], int n) {
     }
 }
 
+
+void find_duplicates_count(int A[], int n) {
+    int total = 0, j = 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] == A[i + 1]) {
+            j = i + 1;
+            total++;
+            while (A[j] == A[i]) j++;
+            cout << "---- " << A[i] << " is appearing " << j - i << " times; " << "----";
+            i = j - 1;
+        }
+    }
+}
+
+
 int main() {
     int A[10] = {3, 6, 8, 8, 10, 12, 15, 15, 15, 20};
     cout << "Duplicate no are: ";
     find_duplicates(A, 10);
+    cout << endl;
+
+    cout << "Duplicate no are: " << endl;
+    find_duplicates_count(A, 10);
     cout << endl;
 }
 
