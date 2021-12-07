67721cdc3891e1a8759b6e384b5e462cddab0dc8
shahriar100 <shahriar.hasan100@gmail.com>
2021-12-07 21:06:10 +0600

    finding duplicate elements with hash table

--- section07_array_adt/student_challenge/find_duplicate_elements.cpp
+++ section07_array_adt/student_challenge/find_duplicate_elements.cpp
@@ -32,6 +32,24 @@ void find_duplicates_count(int A[], int n) {
 }
 
 
+void find_duplicates_count_hashTable(int A[], int n, int high) {
+    int B[high + 1];
+    // initialize the array(hashtable) with zeros
+    for (int i = 0; i <= high; i++) {
+        B[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        B[A[i]]++;
+    };
+
+    for (int i = 1; i <= high; i++) {
+        if (B[i] > 1) {
+            cout << "---- " << i << " is appearing " << B[i] << " times; " << "----";
+        }
+    };
+}
+
 int main() {
     int A[10] = {3, 6, 8, 8, 10, 12, 15, 15, 15, 20};
     cout << "Duplicate no are: ";
@@ -40,6 +58,10 @@ int main() {
 
     cout << "Duplicate no are: " << endl;
     find_duplicates_count(A, 10);
+    cout << endl;
+
+    cout << "Duplicate no are: " << endl;
+    find_duplicates_count_hashTable(A, 10, 20);
     cout << endl;
 }
 
