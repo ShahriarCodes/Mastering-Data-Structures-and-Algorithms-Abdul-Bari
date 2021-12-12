df5929e7030b827c1533d806c783cc5af2ef241c
shahriar100 <shahriar.hasan100@gmail.com>
2021-12-12 19:29:56 +0600

    find_pair_with_sum_k hashtable

--- section07_array_adt/student_challenge/find_pair_of_elements.cpp
+++ section07_array_adt/student_challenge/find_pair_of_elements.cpp
@@ -15,10 +15,27 @@ void find_pair_with_sum_k(int A[], int n, int k) {
 }
 
 
+void find_pair_with_sum_k_hashTable(int A[], int n, int high, int k) {
+    int B[high + 1];
+    // initialize the array(hashtable) with zeros
+    for (int i = 0; i <= high; i++) {
+        B[i] = 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        int diff = k - A[i];
+        if (diff >0 && B[diff] > 0) printf("%d + %d = %d --- ", A[i], diff, k);
+        B[A[i]] = B[A[i]] + 1;
+
+    };
+}
+
 int main() {
     int A[10] = {6, 3, 8, 10, 16, 7, 5, 2, 9, 14};
     find_pair_with_sum_k(A, 10, 10);
     cout << endl;
+    find_pair_with_sum_k_hashTable(A, 10, 16, 10);
+
 
 }
 
